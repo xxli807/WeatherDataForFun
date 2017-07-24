2250eaef12581361f6b0aeda29670adab77eb4f2
lxx871030@gmail.com <lxx871030@gmail.com>
2017-07-24 21:28:42 +1000

    add the asset settings

--- Weather/Controllers/HomeController.cs
+++ Weather/Controllers/HomeController.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using Weather.Models;
 using Weather.Services;
 
 namespace Weather.Controllers
@@ -13,9 +16,12 @@ namespace Weather.Controllers
         // just used as the entry point 
         public ActionResult Index()
         {
-            return View();
+            //use the assertJson to load the js/css
+            var assetsJson = Path.Combine(System.Web.HttpContext.Current.Server.MapPath("~/Dist/"), "webpack-assets.json");
+            var assertModel = JsonConvert.DeserializeObject<WebpackSetting>(System.IO.File.ReadAllText(assetsJson));
+            return View(assertModel);
         }
 
-
+ 
     }
 }
---
+++ Weather/Models/WebpackSetting.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Weather.Models
+{
+    public class WebpackSetting
+    {
+        public Assets App { get; set; }
+
+        public Assets Vendor { get; set; }
+
+        public class Assets
+        {
+            public string Js { get; set; }
+
+            public string Css { get; set; }
+        }
+    }
+}
